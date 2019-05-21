7af3196e826acae034dde28694e07f8e9813ecc9
andrewpsuedonym <j.m.walker@live.co.uk>
2019-05-21 11:46:21 +0100

    added in bots while true loop, ready to begin real world testing

--- bots/bot_main.py
+++ bots/bot_main.py
@@ -3,26 +3,30 @@
 import tweepy
 import config
 import src.Grabber
+import time
 
-#auth = tweepy.OAuthHandler(config.CONSUMER_KEY, config.CONSUMER_SECRET)
-#auth.set_access_token(config.ACCESS_KEY, config.ACCESS_SECRET)
-#API = tweepy.API(auth)
+auth = tweepy.OAuthHandler(config.CONSUMER_KEY, config.CONSUMER_SECRET)
+auth.set_access_token(config.ACCESS_KEY, config.ACCESS_SECRET)
+API = tweepy.API(auth)
 
 if __name__ == '__main__':
-    print('hi')
+    print('Running bot')
     g = Grabber.Grabber()
-
     verse = ''
-    for book_num in range(1,13):
-        sec = 1
-        verse = g.get_verse(book_num, sec)
-        if verse.split('\n')[0].strip() == 'THE ARGUMENT':
-            verse = 'BOOK ' + str(book_num) + '\n' + verse
-            print(len(verse))
-        while verse != '':
-            sec += 1
+    i = 0
+    while True:
+        print('Bot has completed', i, 'readings')
+        for book_num in range(1,13):
+            sec = 1
             verse = g.get_verse(book_num, sec)
-            if verse.split('\n')[0].strip() == 'THE END':
-                break
-
-    print(verse)
+            if verse.split('\n')[0].strip() == 'THE ARGUMENT':
+                verse = 'BOOK ' + str(book_num) + '\n' + verse
+                API.update_status(verse)
+                time.sleep(3600)
+            while verse != '':
+                sec += 1
+                verse = g.get_verse(book_num, sec)
+                API.update_status(verse)
+                time.sleep(3600)
+                if verse.split('\n')[0].strip() == 'THE END':
+                    break
