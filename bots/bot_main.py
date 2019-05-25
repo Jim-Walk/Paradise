3aa5974df5079b8d67c94f5cf983a8aa40b89492
andrewpsuedonym <j.m.walker@live.co.uk>
2019-05-25 20:21:15 +0100

    Fixed bugs, and added code to enable bot to resume from where it left off

--- bots/bot_main.py
+++ bots/bot_main.py
@@ -4,29 +4,62 @@ import tweepy
 import config
 import src.Grabber as Grabber
 import time
+import sys
 
 auth = tweepy.OAuthHandler(config.CONSUMER_KEY, config.CONSUMER_SECRET)
 auth.set_access_token(config.ACCESS_KEY, config.ACCESS_SECRET)
 API = tweepy.API(auth)
 
+# Sometimes a section is outside of a book, and then we return blank
+# sometimes a section is at the end of the book, and then we need to know
+# so we can break the loop
+def book_end(verse):
+    if verse == '':
+        return False
+    for line in verse.split('\n'):
+        if 'Book.' in line.split():
+            return True
+    return False
+
 if __name__ == '__main__':
+    debug = False
+    if len(sys.argv) > 1:
+        if sys.argv[1] == '-d':
+            debug = True
+            print('Running in debug mode')
     print('Running bot')
     g = Grabber.Grabber()
     verse = ''
     i = 0
     while True:
         print('Bot has completed', i, 'readings')
-        for book_num in range(1,13):
-            sec = 1
+        book_num, sec = g.get_recent_bk_sec()
+        while book_num < 13:
             verse = g.get_verse(book_num, sec)
+            # Essentially a do while to make sure we include
+            # the book header
             if verse.split('\n')[0].strip() == 'THE ARGUMENT':
                 verse = 'BOOK ' + str(book_num) + '\n' + verse
-                API.update_status(verse)
-                time.sleep(1800)
-            while verse != '':
+                if debug:
+                    print(verse)
+                    time.sleep(5)
+                else:
+                    API.update_status(verse)
+                    time.sleep(1800)
+            while verse != '' and not book_end(verse):
                 sec += 1
                 verse = g.get_verse(book_num, sec)
-                API.update_status(verse)
-                time.sleep(1800)
+                if debug:
+                    print(verse)
+                else:
+                    API.update_status(verse)
+                    time.sleep(1800)
+                # Poem End
                 if verse.split('\n')[0].strip() == 'THE END':
+                    book_num = 1
                     break
+                # Most books have this string in their last phrase,
+                # as in, the end of the sixth book
+            book_num += 1
+            sec = 1
+        i += 1
--- bots/src/Grabber.py
+++ bots/src/Grabber.py
@@ -1,4 +1,11 @@
 import re
+import tweepy
+import config
+import time
+
+auth = tweepy.OAuthHandler(config.CONSUMER_KEY, config.CONSUMER_SECRET)
+auth.set_access_token(config.ACCESS_KEY, config.ACCESS_SECRET)
+API = tweepy.API(auth)
 
 BOOKS = re.compile('BOOK \d')
 
@@ -19,16 +26,49 @@ class Grabber():
                 self.book_idxs += [i]
             i += 1
 
+    def get_most_recent_verse(self):
+        tweet_list = API.user_timeline(count=1, tweet_mode='extended')
+        return tweet_list[0]._json['full_text']
+
+    # Returns book and section of the most recent tweet
+    def get_recent_bk_sec(self):
+        bk = 1
+        sec = 1
+        recent_verse = self.get_most_recent_verse()
+        verse = ''
+        while recent_verse.strip() != verse.strip():
+            verse = self.get_verse(bk,sec)
+            if verse == '':
+                break
+            sec += 1
+            for line in verse.split('\n'):
+                line = line.strip()
+                if BOOKS.match(line):
+                    bk += 1
+        return bk, sec
+
+
+    # return verse for given book and section.
+    # if section is not in book then we return a blank
     def get_verse(self, bk, sec):
         i = self.book_idxs[bk] + 1
         char_limit = 190
         char_count = 0; sec_count = 0
-        while sec_count < sec:
+        sec_in_book = True
+        while sec_count < sec and sec_in_book:
             verse = ''
             while char_count < char_limit:
                 if i >= len(self.poem):
                     break
                 if BOOKS.match(self.poem[i]):
+                    # if this section is the last book
+                    # the current verse  is the end of the book, 
+                    # we can return it safely
+                    if verse != '':
+                        for line in verse.split('\n'):
+                            if 'Book.' in line.split():
+                                return verse
+                    sec_in_book = False
                     break
                 char_i = 0
                 while char_i < len(self.poem[i]):
@@ -39,4 +79,7 @@ class Grabber():
             sec_count += 1
             char_count = 0
 
-        return verse
+        if sec_in_book:
+            return verse
+        else:
+            return ''
